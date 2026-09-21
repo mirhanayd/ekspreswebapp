dd9ff30906a6a09b057f70407f22cfccd4fc4f9f
Yunus Mirhan Aydınlı <128381182+mirhanayd@users.noreply.github.com>
2026-09-21 11:17:05 +0300

    feat: export shared driver backend

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,3 +1,4 @@
 export { createDatabaseClient } from './client.js';
 export * as schema from './schema/index.js';
 export { validateConfig, type DatabaseConfig } from './config.js';
+export * from './server/driver-backend.js';
